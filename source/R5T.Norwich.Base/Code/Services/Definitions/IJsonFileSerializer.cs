7bfa58651b59410dc0a07f66fe23096774b60fee
MinexAutomation <minex.automation@gmail.com>
2019-12-28 17:34:08 -0800

    Added IJsonFileSerializer and ISynchronousJsonFileSerializer service definition interface types.

---
+++ source/R5T.Norwich.Base/Code/Services/Definitions/IJsonFileSerializer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Threading.Tasks;
+
+
+namespace R5T.Norwich
+{
+    public interface IJsonFileSerializer
+    {
+        Task<T> DeserializeAsync<T>(string jsonFilePath);
+        Task SerializeAsync<T>(string jsonFilePath, T @object, bool overwrite = true);
+    }
+}
---
+++ source/R5T.Norwich.Base/Code/Services/Definitions/ISynchronousJsonFileSerializer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+
+namespace R5T.Norwich
+{
+    public interface ISynchronousJsonFileSerializer
+    {
+        T Deserialize<T>(string jsonFilePath);
+        void Serialize<T>(string jsonFilePath, T @object, bool overwrite = true);
+    }
+}
