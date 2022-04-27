0d2a6dffa59f13ff83ec238533b756a7b966623e
davidcoats <david@getsafetycone.com>
2022-04-27 15:33:18 -0700

    Interim changes.

--- source/R5T.Norwich.Base/Code/Services/Definitions/IJsonFileSerializer.cs
+++ source/R5T.Norwich.Base/Code/Services/Definitions/IJsonFileSerializer.cs
@@ -1,10 +1,10 @@
-﻿using System;
-using System.Threading.Tasks;
+using System;
+using System.Threading.Tasks;using R5T.T0064;
 
 
 namespace R5T.Norwich
-{
-    public interface IJsonFileSerializer
+{[ServiceDefinitionMarker]
+    public interface IJsonFileSerializer:IServiceDefinition
     {
         Task<T> DeserializeAsync<T>(string jsonFilePath);
         Task SerializeAsync<T>(string jsonFilePath, T @object, bool overwrite = true);
--- source/R5T.Norwich.Base/Code/Services/Definitions/ISynchronousJsonFileSerializer.cs
+++ source/R5T.Norwich.Base/Code/Services/Definitions/ISynchronousJsonFileSerializer.cs
@@ -1,9 +1,9 @@
-﻿using System;
+using System;using R5T.T0064;
 
 
 namespace R5T.Norwich
-{
-    public interface ISynchronousJsonFileSerializer
+{[ServiceDefinitionMarker]
+    public interface ISynchronousJsonFileSerializer:IServiceDefinition
     {
         T Deserialize<T>(string jsonFilePath);
         void Serialize<T>(string jsonFilePath, T @object, bool overwrite = true);
